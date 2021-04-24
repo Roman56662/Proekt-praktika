5612b91c5f9609cf2e8acc1227eddb5fa6f5a9c6
Stanislav <stanislavbajkov2@gmail.com>
2021-04-24 23:29:17 +0300

    return Calendar

--- src/components/base/Calendar/Calendar.jsx
+++ src/components/base/Calendar/Calendar.jsx
@@ -1,39 +1,39 @@
-// import React, { useCallback, useState } from 'react';
-// import {Calendar} from 'react-calendar';
-// // import 'react-calendar/dist/Calendar.css';
-// import cn from 'classnames/bind';
+import React, { useCallback, useState } from 'react';
+import {Calendar} from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
+import cn from 'classnames/bind';
 
-// //Styles
-// import styles from './styles.styl';
-// const cx = cn.bind(styles);
+//Styles
+import styles from './styles.styl';
+const cx = cn.bind(styles);
 
-// export const CalendarComponent = ({getDayArrived, getDayDeparted, getMonthArrived, getMonthDeparted}) => {
+export const CalendarComponent = ({getDayArrived, getDayDeparted, getMonthArrived, getMonthDeparted}) => {
 
-//   const [dateArrived, setDateArrived] = useState(new Date())
-//   const [dateDeparted, setDateDeparted] = useState(new Date())
+  const [dateArrived, setDateArrived] = useState(new Date())
+  const [dateDeparted, setDateDeparted] = useState(new Date())
 
-//   let today
+  let today
 
-//   const onChangeArrived = useCallback( (dateArrived) => {
-//     setDateArrived(dateArrived);
-//     today = dateArrived.getDate()
-//     getDayArrived(dateArrived.getDate(dateArrived.setDate(today)))
-//     getMonthArrived(dateArrived.getMonth())
-//   },[dateArrived]) 
+  const onChangeArrived = useCallback( (dateArrived) => {
+    setDateArrived(dateArrived);
+    today = dateArrived.getDate()
+    getDayArrived(dateArrived.getDate(dateArrived.setDate(today)))
+    getMonthArrived(dateArrived.getMonth())
+  },[dateArrived]) 
 
-//   const onChangeDeparted = useCallback( (dateDeparted) => {
-//     setDateDeparted(dateDeparted);
-//     today = dateDeparted.getDate()
-//     getDayDeparted(dateDeparted.getDate(dateDeparted.setDate(today)))
-//     getMonthDeparted(dateDeparted.getMonth())
-//   },[dateDeparted])   
+  const onChangeDeparted = useCallback( (dateDeparted) => {
+    setDateDeparted(dateDeparted);
+    today = dateDeparted.getDate()
+    getDayDeparted(dateDeparted.getDate(dateDeparted.setDate(today)))
+    getMonthDeparted(dateDeparted.getMonth())
+  },[dateDeparted])   
   
-//   return (
-//       <div className={cx('calendar')}>
-//         <Calendar  minDate={new Date()} maxDate={new Date(2022, 0, 1, 0, 0, 0, 0)} onChange={onChangeArrived} value={dateArrived} className={cx('react-calendar')}/>
-//         {/* <Calendar  minDate={new Date(dateArrived)} maxDate={new Date(2022, 0, 1, 0, 0, 0, 0)} onChange={onChangeDeparted} value={dateDeparted} className={cx('react-calendar')}/> */}
+  return (
+      <div className={cx('calendar')}>
+        <Calendar  minDate={new Date()} maxDate={new Date(2022, 0, 1, 0, 0, 0, 0)} onChange={onChangeArrived} value={dateArrived} className={cx('react-calendar')}/>
+        {/* <Calendar  minDate={new Date(dateArrived)} maxDate={new Date(2022, 0, 1, 0, 0, 0, 0)} onChange={onChangeDeparted} value={dateDeparted} className={cx('react-calendar')}/> */}
         
-//       </div>
-//   );
-// }
+      </div>
+  );
+}
 
--- src/components/base/Calendar/Calendar.stories.jsx
+++ src/components/base/Calendar/Calendar.stories.jsx
@@ -1,49 +1,49 @@
-// import React, { useState, useCallback } from 'react';
-// import { CalendarComponent } from './Calendar';
+import React, { useState, useCallback } from 'react';
+import { CalendarComponent } from './Calendar';
 
-// import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs, number } from '@storybook/addon-knobs';
 
-// export default {
-//   title: 'CalendarComponent',
-//   decorators: [withKnobs],
-//   parameters: {
-//     docs: {
-//     },
-//   },
-// };
+export default {
+  title: 'CalendarComponent',
+  decorators: [withKnobs],
+  parameters: {
+    docs: {
+    },
+  },
+};
 
-// const CalendarComponentData = () => {
-//   const [dayArrived, setDayArrived] = useState()
-//   const [monthArrived, setMonthArrived] = useState()
+const CalendarComponentData = () => {
+  const [dayArrived, setDayArrived] = useState()
+  const [monthArrived, setMonthArrived] = useState()
 
-//   const [dayDeparted, setDayDeparted] = useState()
-//   const [monthDeparted, setMonthDeparted] = useState()
+  const [dayDeparted, setDayDeparted] = useState()
+  const [monthDeparted, setMonthDeparted] = useState()
 
-//   const monthArr = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
-//                     'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'] 
+  const monthArr = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                    'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'] 
 
-//   const getDayArrived = useCallback( (value) => {
-//     setDayArrived(value);
-//   })
+  const getDayArrived = useCallback( (value) => {
+    setDayArrived(value);
+  })
 
-//   const getDayDeparted = useCallback( (value) => {
-//     setDayDeparted(value);
-//   })
+  const getDayDeparted = useCallback( (value) => {
+    setDayDeparted(value);
+  })
 
-//   const getMonthArrived = useCallback( (value) => {
-//     setMonthArrived(monthArr[value]);
-//   })
+  const getMonthArrived = useCallback( (value) => {
+    setMonthArrived(monthArr[value]);
+  })
 
-//   const getMonthDeparted = useCallback( (value) => {
-//     setMonthDeparted(monthArr[value]);
-//   })
+  const getMonthDeparted = useCallback( (value) => {
+    setMonthDeparted(monthArr[value]);
+  })
 
-//   return (
-//     <div>
-//     <CalendarComponent getDayArrived={getDayArrived} getDayDeparted={getDayDeparted} getMonthArrived={getMonthArrived} getMonthDeparted={getMonthDeparted} />
-//     <p>{dayArrived + ' ' + monthArrived }</p>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+    <CalendarComponent getDayArrived={getDayArrived} getDayDeparted={getDayDeparted} getMonthArrived={getMonthArrived} getMonthDeparted={getMonthDeparted} />
+    <p>{dayArrived + ' ' + monthArrived }</p>
+    </div>
+  );
+};
 
-// export const example = () => <CalendarComponentData/>
+export const example = () => <CalendarComponentData/>
